39d0561654f531071ed906ed857c0daf983fa54a
Wasif Baliyan <w.baliyan@gmail.com>
2020-12-24 17:55:14 +0530

    feat: logic fucntion for cash counting and styling using plain css and add dark mode also

--- js/app.js
+++ js/app.js
@@ -1,7 +1,70 @@
-function cashRegister(notesAccepted, billAmount, cashPaid) {
-  if (cashPaid < billAmount) {
-    return "Cash paid is less than the bill amount!";
+const notesAccepted = [2000, 500, 200, 100, 50, 20, 10, 5, 1];
+const form = document.querySelector(".user-input");
+const bill = document.querySelector(".bill-amount");
+const cash = document.querySelector(".cash-amount");
+const error = document.querySelector(".error");
+const noteList = document.querySelector(".note-list");
+const result = document.querySelector(".result-display");
+const toggle = document.querySelector(".toggle-mode");
+const root = document.querySelector(":root");
+
+let darkMode = true;
+toggle.addEventListener("click", () => {
+  darkMode = !darkMode;
+  if (darkMode) {
+    toggle.textContent = "☀️";
+    root.style.setProperty("--color-primary", "#ffbc42");
+    root.style.setProperty("--color-secondary", " #000001");
+  } else {
+    toggle.textContent = "⛅";
+    root.style.setProperty("--color-primary", " #000001");
+    root.style.setProperty("--color-secondary", "#ffbc42");
   }
+});
+
+result.style.display = "none";
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  result.style.display = "";
+  if (Number(cash.value) < Number(bill.value)) {
+    noteList.style.display = "none";
+    error.textContent = "Cash paid is less than the bill amount!";
+  } else if (Number(cash.value) === Number(bill.value)) {
+    noteList.style.display = "none";
+    error.textContent = "Yayyy! You paid the exact bill amount!";
+  } else {
+    noteList.style.display = "";
+    error.textContent =
+      "Amount To Collect: " +
+      (Number(cash.value) - Number(bill.value)) +
+      "Rupees";
+    const notes = cashRegister(
+      notesAccepted,
+      Number(bill.value),
+      Number(cash.value)
+    );
+    noteList.innerHTML = Object.keys(notes)
+      .map((note) => {
+        return (
+          "<li>" +
+          "<span>" +
+          note +
+          "</span>" +
+          "-" +
+          "<span>" +
+          notes[note] +
+          "</span>" +
+          "</li>"
+        );
+      })
+      .join("");
+  }
+  bill.value = "";
+  cash.value = "";
+});
+
+function cashRegister(notesAccepted, billAmount, cashPaid) {
   const cashToReturn = cashPaid - billAmount;
   const notes = {};
   let bill = cashToReturn;
@@ -16,11 +79,3 @@ function cashRegister(notesAccepted, billAmount, cashPaid) {
   }
   return notes;
 }
-
-const notesAccepted = [2000, 500, 200, 100, 50, 20, 10, 5, 1];
-
-const billAmount = 33322;
-const cashPaid = 89998;
-console.log(cashPaid - billAmount);
-
-console.log(cashRegister(notesAccepted, billAmount, cashPaid));
